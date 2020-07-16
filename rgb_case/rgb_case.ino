a11367dddb1761614430e51a4c4867d73f109b07
Mout Pessemier <moutpessemier@hotmail.com>
2020-07-16 15:07:53 +0200

    V1.0.4 - updated sketsch, no servos needed

--- rgb_case/rgb_case.ino
+++ rgb_case/rgb_case.ino
@@ -1,43 +1,42 @@
-#include <Servo.h>
+// Written for Arduino Uno
 
-// My 3 turn knobs
-Servo R;
-Servo G;
-Servo B;
+// Just change the pins and the sketch will do the rest
 
-const int red_light_pin = D5;
-const int green_light_pin = D6;
-const int blue_light_pin = D7;
+// The input of the 3 turn knobs
+const int potpinR = A0;
+const int potpinG = A1;
+const int potpinB = A2;
+
+// The output for the 3 colors
+const int red_light_pin = 11;
+const int green_light_pin = 10;
+const int blue_light_pin = 9;
 
 void setup() {
-  // Attach the knobs to the pins
-  R.attach(D0);
-  G.attach(D1);
-  B.attach(D2);
-
   // Prepare output pins for RGB
-  pinMode(D5,OUTPUT);
-  pinMode(D6,OUTPUT);
-  pinMode(D7,OUTPUT);
-
+  pinMode(red_light_pin, OUTPUT);
+  pinMode(green_light_pin, OUTPUT);
+  pinMode(blue_light_pin, OUTPUT);
 }
 
 void loop() {
-  int r = getServoValue(D0);
-  int g = getServoValue(D1);
-  int b = getServoValue(D2);
+  // Read the 3 values
+  int r = getPotentiaValue(potpinR);
+  int g = getPotentiaValue(potpinG);
+  int b = getPotentiaValue(potpinB);
+  // Set color of LEDs
   setColor(r,g,b);
 }
 
 // reads the value of the knob and maps it to an rgb value
-int getServoValue(int pin) {
+int getPotentiaValue(int pin) {
   int val = analogRead(pin);
   val = map(val, 0, 1023, 0, 255);
   return val;
 }
 
 // Sets the color for the different RGB LEDs
-void setColor(red,green,blue) {
+void setColor(int red, int green, int blue) {
   analogWrite(red_light_pin, red);
   analogWrite(green_light_pin, green);
   analogWrite(blue_light_pin, blue);
